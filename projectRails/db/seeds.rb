f41ccaacfe72f9544f7ea5e134171fa4de43b9ce
miguel <miguelfernandomacias@gmail.com>
2022-12-08 02:14:49 -0400

    added seed

--- projectRails/db/seeds.rb
+++ projectRails/db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Competition.destroy_all
+
+Competition.create!([{
+    team: "Devastating Donkeys",
+    mp: 3,
+    w: 2,
+    d: 1,
+    l: 0,
+    p: 7
+},
+{
+    team: "Allegoric Alaskans",
+    mp: 3,
+    w: 2,
+    d: 0,
+    l: 1,
+    p: 6
+},
+{
+    team: "Blithering Badgers",
+    mp: 3,
+    w: 1,
+    d: 0,
+    l: 2,
+    p: 3
+},
+{
+    team: "Courageous Californians",
+    mp: 3,
+    w: 0,
+    d: 1,
+    l: 2,
+    p: 1
+}])
